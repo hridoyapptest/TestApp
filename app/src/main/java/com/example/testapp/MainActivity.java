3777376b4d091fc91f671758e5e2c0c37e05b018
hrid0y <hridoyapptest@gmail.com>
2021-07-27 21:36:12 +0600

    Initial Upload of the Project

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Welcome to TestApp!", Toast.LENGTH_SHORT);
+    }
+}
